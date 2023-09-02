d494c99a1d23a775052c19bcc35c1e719beb1a8c
habutaso <habutaso@gmail.com>
2023-09-02 08:15:28 +0100

    fix

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from tflite_support.task import processor, vision
 
 import visualize
 from camera import Camera
-from constants import DEFAULT_MAX_RESULTS, DEFAULT_MODEL_FILENAME, DEFAULT_SCORE_THRESHOLD, DEFAULT_THREADS
+from constants import DEFAULT_MAX_RESULTS, DEFAULT_MODEL_FILENAME, DEFAULT_SCORE_THRESHOLD, DEFAULT_THREADS, DEFAULT_FRAME_WIDTH_HALF
 from coordinate import Coordinate, DetectionCoordinate
 from detector import Detector
 
 MODEL_FILENAME = "efficientdet_lite0.tflite"
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(27, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
@@ -28,7 +29,19 @@ px_2.start(0)
 
 def move_motor(vector: Tuple[int, int]):
     print(vector)
-    pass
+    duty_x = (10 + int(abs(vector[0]) / DEFAULT_FRAME_WIDTH_HALF * 100) / 5)
+    if vector[0] > 0:
+        px_1.ChangeDutyCycle(duty_x)
+        px_2.ChangeDutyCycle(0)
+    elif vector[0] == 0:
+        px_1.ChangeDutyCycle(100)
+        px_2.ChangeDutyCycle(100)
+    else:
+        px_1.ChangeDutyCycle(0)
+        px_2.ChangeDutyCycle(duty_x)
+        
+    # print(duty_x)
+    # px_1.ChangeDutyCycle(duty_x)
 
 
 def main():
@@ -64,13 +77,15 @@ def main():
         cv2.imshow("detector", image)
 
         if len(detection_result.detections) < 1:
+            GPIO.output(27, False)
             continue
 
         test_detection = detection_result.detections[0]
         coordinate = DetectionCoordinate(test_detection)
         h, w, _ = image.shape
-        point_diff = visualize.get_diff_from_center(Coordinate(w, h), coordinate.gravity)
+        point_diff = visualize.get_diff_from_center(Coordinate(w // 2, h // 2), coordinate.gravity)
         move_motor(point_diff)
+        GPIO.output(27, True)
 
         if cv2.waitKey(1) == "q":
             break
